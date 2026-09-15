c1005c4c9cd2553175d32a65650d07960e2a2bb4
Ramith <ramithrlal@gmail.com>
2026-09-15 14:20:37 +0530

    chore: add white background to favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -97,7 +97,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/logo.png", type: "image/png" },
+      { rel: "icon", href: "/favicon.png", type: "image/png" },
     ],
 
   }),
